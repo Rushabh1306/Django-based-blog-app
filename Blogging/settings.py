33d8cca6fc84567188d01fba925210c96763726a
Rushabhsoni1306 <64094208+Rushabh1306@users.noreply.github.com>
2021-05-30 00:02:50 +0530

    Updated Profiles

--- Blogging/settings.py
+++ Blogging/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '6*l-2&kp-y8oz96#-4wz252be)rzcw)qgrbicvg-q40p^z^iz)'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
-ALLOWED_HOSTS = ['ddjangoblog.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -144,8 +144,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-# os.environ['EMAIL_PASS']='ifudrlndhmtskukz'
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS') 
